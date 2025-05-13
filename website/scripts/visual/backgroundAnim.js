8fb29695b0069ef476cb80b575bd006ee3f24976
Viktor <viktortylus@gmail.com>
2025-05-13 14:39:01 +0200

    fixed some frontend components

--- website/scripts/visual/backgroundAnim.js
+++ website/scripts/visual/backgroundAnim.js
@@ -1,12 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     const mainSection = document.querySelector("img");
+    mainSection.classList.add("fade");
     const images = 8;
+    let currentIndex = Math.floor(Math.random() * images + 1);
+    mainSection.src = `../assets/backgroundImages/background${currentIndex}.png`;
+    
     function changeBackground() {
+        mainSection.style.opacity = 0;
         setTimeout(() => {
             currentIndex = Math.floor(Math.random() * images + 1);
-            mainSection.src = `../assets/backgroundImages/background${[currentIndex]}.png`;
-        }, 1000); 
+            mainSection.src = `../assets/backgroundImages/background${currentIndex}.png`;
+        }, 1000);
     }
 
-    setInterval(changeBackground, 2000);
+    mainSection.addEventListener('load', function() {
+        mainSection.style.opacity = 1;
+    });
+
+    setInterval(changeBackground, 3000);
 });
